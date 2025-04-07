4257d77f51636a2b94db247f7cf05c07c94172d0
sircylee <kylie.x.cylee.technology@gmail.com>
2025-04-08 01:17:22 +0200

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,6 +1,6 @@
 async function aliveCommand(sock, chatId) {
     try {
-        const message = `*🤖 Knight Bot is Active!*\n\n` +
+        const message = `*👸❤️ Mommyy is Active!*\n\n` +
                        `*Version:* 1.0.0\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
