a920ca615b6bb61117321f0f718da83a9d572023
sircylee <kylie.x.cylee.technology@gmail.com>
2025-04-08 09:10:46 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Cadillac Cylee', // Your name
+  packname: 'errrbodyhatescylee',
+  author: 'Kylie 👸❤️',
+  botName: "",
+  botOwner: 'Cadillac Cylee 👸❤️', // Your name
   ownerNumber: '27835039725', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
